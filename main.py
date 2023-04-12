487c09986cee39100d62f94f6f43a48b51d723a2
exterminateddd <daniil190107@gmail.com>
2023-04-12 23:14:36 +0300

    add basic functionality

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from plotly.express import pie
+from pandas import DataFrame
+from requests import get
 
-st.title('Hello world111')
+st.title('Распределение книг по жанрам')
+st.markdown('Перейти к [Книжному каталогу](http://1.cshse.beget.tech/books "click")')
+
+api_resp = get('http://1.cshse.beget.tech/action/get_genres_stats')
+df = DataFrame(data=dict(api_resp.json()))
+chart = pie(
+        df,
+        title='Круговая диаграмма',
+        values='genre_weights',
+        names='genre_names'
+    )
+
+st.plotly_chart(chart)
